fe3d2eb61071c791a19262288f8678a7653af289
Aurelio C. Remonda <aurelioremonda@gmail.com>
2015-11-29 01:24:36 -0300

    agregado modulo decodificador de instrucciones, extension de signo y unidad de control

---
+++ Arquitectura_TPF/control_unit.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:31:18 11/28/2015 
+// Design Name: 
+// Module Name:    control_unit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module control_unit(
+		input wire [5:0] opcode,
+		input clk,
+		input ena,
+		input rst,
+		output reg rd,
+		output reg aluSrc,
+		output reg mem_to_reg,
+		output reg reg_write,	
+		output reg mem_read,
+		output reg mem_write,
+		output reg branch,
+		output reg next_mem_read,
+		output reg [5:0]aluOp,
+    );
+		reg next_rd;
+		reg next_aluSrc;
+		reg next_mem_to_reg;
+		reg next_reg_write;
+		reg next_mem_read;
+		reg next_mem_write;
+		reg next_branch;
+		reg next_mem_read;
+		reg [5:0]next_aluOp;
+		
+		
+		//next state logic
+		always @*
+		begin
+		aluOp = next_aluOp;
+		aluSrc = next_aluSrc;
+		branch = next_branch;
+		mem_to_reg = next_mem_to_reg;
+		mem_write = next_mem_write;
+		rd = next_rd;
+		reg_write = next_reg_write;
+		case (opcode)
+		6'b000000://R o J instructions
+		reg_dest = 1; //falta agregar
+		aluSrc = 0;
+		mem_to_reg = 0;
+		reg_write = 1;
+		aluOp = instruction[5:0];
+		branch = 0;
+		default: 
+			
+			
+		endcase
+
+endmodule
---
+++ Arquitectura_TPF/sig_extend.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:05:17 11/28/2015 
+// Design Name: 
+// Module Name:    sig_extend 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sig_extend(
+	input [15:0]reg_in,
+	output reg signed [31:0]reg_out,
+	input wire clk,
+	input wire reset
+    );
+	
+	always @(posedge clk)
+	begin
+		if (reset)
+		begin
+			reg_out <= 0; 
+		end
+		else
+		begin 
+			reg_out <= $signed(reg_in);
+		end
+		
+	
+		
+	end
+endmodule
